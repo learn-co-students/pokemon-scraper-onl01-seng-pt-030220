61c45aaa94a2b8ede346fa2014d4c84d174df9ae
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-03 17:44:29 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,9 +11,7 @@ class Pokemon
     sql=<<-SQL
        INSERT INTO pokemon (name,type) VALUES(?,?)
     SQL
-
     db.execute(sql,name,type)
-    @id=db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
 
   def self.find(id,db)
