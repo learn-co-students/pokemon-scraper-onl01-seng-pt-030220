fe47bdff553863adacd05ba87e6dc9af27184bc4
VQuine <01010110.v@gmail.com>
2020-06-07 17:01:58 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    #KEYWORD ARGUMENTS PARAMATERS
+    def initialize(id: , name: , type: , db: )
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def save
+        # save instance of Pokemon
+        # VALIDATE ID
+
+
+    end
+
 end
