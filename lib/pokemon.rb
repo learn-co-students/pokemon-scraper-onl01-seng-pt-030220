4e69ad82135eef8029a40e90d4cf9bd8810adfec
VQuine <01010110.v@gmail.com>
2020-06-08 15:31:50 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,8 @@
+require 'pry'
 class Pokemon
-    attr_accessor :id, :name, :type, :db
+
+    attr_accessor :name, :type, :db
+    attr_reader :id
 
     #KEYWORD ARGUMENTS PARAMATERS
     def initialize(id: , name: , type: , db: )
@@ -9,11 +12,15 @@ class Pokemon
         @db = db
     end
 
-    def save
-        # save instance of Pokemon
-        # VALIDATE ID
-
-
+    def self.save(name, type, db)
+        sql = "INSERT INTO pokemon (name, type) VALUES (?, ?);"
+        db.execute(sql, name, type)
     end
 
-end
+    def self.find(id, datab)
+        sql = "SELECT * FROM pokemon WHERE id = ?;"
+        blip = datab.execute(sql, id)[0]
+        Pokemon.new(id: blip[0], name: blip[1], type: blip[2], db: datab)
+    end
+
+end #<---CLASSend
